b27e1c168fd697a358432c3ed10c96f37987d0c8
RMondGP <practical4n6@gmail.com>
2025-11-07 20:06:30 -0500

    static change in output of site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://rmondgp.github.io',
   base: '/The-Forensics-Way',
   integrations: [
